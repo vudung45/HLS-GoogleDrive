33159a7b29901f0ed42d840fbb86b511c0ee2816
vudung45 <vudung45@yahoo.com>
2020-03-21 01:29:53 -0500

    no trim

--- app/media_utils/hlsconverter.js
+++ app/media_utils/hlsconverter.js
@@ -106,7 +106,7 @@ export default class HLSConverter {
                     this.m3u8Header = chunkLines.slice(0, chunkLines.length - 2).join("\n");
 
                 let extinf = chunkLines[chunkLines.length - 2].match(/#EXTINF:(.*),/)[1];
-                let chunkPath = chunkLines[chunkLines.length - 1].trim();
+                let chunkPath = chunkLines[chunkLines.length - 1];
                 this.processedChunk.push({
                     "status": 1,
                     "extinf": extinf,
