f9b9ebe0f995da5d9cba55fa3141efc047bcd3b8
vudung45 <vudung45@yahoo.com>
2020-03-21 03:52:23 -0500

    increase delay

--- app/google/accounts.js
+++ app/google/accounts.js
@@ -17,7 +17,7 @@ async function retryApi(apiCoroutine) {
     })
 }
 
-const DELAY_TIME = 200; // 100ms
+const DELAY_TIME = 500; // 100ms
 
 export class Account {
     constructor(identifier, authClient) {
