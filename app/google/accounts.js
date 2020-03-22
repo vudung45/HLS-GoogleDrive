3f973d87035db4b8c643990e2481f457656d469b
vudung45 <vudung45@yahoo.com>
2020-03-21 20:49:58 -0500

    error handling

--- app/google/accounts.js
+++ app/google/accounts.js
@@ -17,7 +17,7 @@ async function retryApi(apiCoroutine) {
     }, {delay: 1000})
 }
 
-const DELAY_TIME = 100; // 100ms
+const DELAY_TIME = 500; // 100ms
 
 export class Account {
     constructor(identifier, authClient) {
