de0497d7a924d033550b892385c8c5b053087e75
vudung45 <vudung45@yahoo.com>
2020-03-23 00:45:08 -0500

    1 Worker, 3 jobs per worker

--- app/workers/convert_hls_worker.js
+++ app/workers/convert_hls_worker.js
@@ -21,14 +21,14 @@ let REDIS_URL = process.env.REDIS_URL || "redis://127.0.0.1:6379";
 
 // Spin up multiple processes to handle jobs to take advantage of more CPU cores
 // See: https://devcenter.heroku.com/articles/node-concurrency for more info
-let workers = process.env.WEB_CONCURRENCY || 2;
+let workers = process.env.WEB_CONCURRENCY || 1;
 
 
 // The maxium number of jobs each worker should process at once. This will need
 // to be tuned for your application. If each job is mostly waiting on network 
 // responses it can be much higher. If each job is CPU-intensive, it might need
 // to be much lower.
-let maxJobsPerWorker = 1;
+let maxJobsPerWorker = 3;
 
 var filesToCleanUp = [];
 
